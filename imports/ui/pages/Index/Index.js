84b67dcde283f4ea9b0aacccf583c4c06349de33
Brandon Howard <akhockey21@live.com>
2017-10-14 21:19:45 -0400

    Add Material to Homepage

--- imports/ui/pages/Index/Index.js
+++ imports/ui/pages/Index/Index.js
@@ -1,23 +1,35 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Grid, Breadcrumb, Button } from 'react-bootstrap';
+import Paper from 'material-ui/Paper'
 
 import './Index.scss';
 
 const Index = () => (
-  <div className="Index">
-    <img
-      src="https://s3-us-west-2.amazonaws.com/cleverbeagle-assets/graphics/email-icon.png"
-      alt="Clever Beagle"
-    />
-    <h1>Pup</h1>
-    <p>A boilerplate for products.</p>
-    <div>
-      <Button href="http://cleverbeagle.com/pup">Read the Docs</Button>
-      <Button href="https://github.com/cleverbeagle/pup"><i className="fa fa-star" /> Star on GitHub</Button>
-    </div>
-    <footer>
-      <p>Need help and want to stay accountable building your product? <a href="http://cleverbeagle.com?utm_source=pupappindex&utm_campaign=oss">Check out Clever Beagle</a>.</p>
-    </footer>
+  <div>
+    <Paper zDepth={1}>
+      <Breadcrumb>
+        <Breadcrumb.Item active>
+          Home
+        </Breadcrumb.Item>
+      </Breadcrumb>
+    </Paper>
+    <Grid>
+      <Paper className="Index" zDepth={1}>
+        <img
+          src="https://s3-us-west-2.amazonaws.com/cleverbeagle-assets/graphics/email-icon.png"
+          alt="Clever Beagle"
+        />
+        <h1>Pup</h1>
+        <p>A boilerplate for products.</p>
+        <div>
+          <Button href="http://cleverbeagle.com/pup">Read the Docs</Button>
+          <Button href="https://github.com/cleverbeagle/pup"><i className="fa fa-star" /> Star on GitHub</Button>
+        </div>
+        <footer>
+          <p>Need help and want to stay accountable building your product? <a href="http://cleverbeagle.com?utm_source=pupappindex&utm_campaign=oss">Check out Clever Beagle</a>.</p>
+        </footer>
+      </Paper>
+    </Grid>
   </div>
 );
 
